8969e2011fb0dbea697329a017f21a68b7a4c785
justmarvis <playdeadcanyon@gamil.com>
2022-08-27 01:51:34 -0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div className="design">
+        <p className="title">Vector Designs</p>
+        <p className="stamp">Dev-VER <br /> Design</p>
+        <div className="collage">
+          <div className="first grid">
+            <img src="https://cdn.glitch.global/6a19564e-7f95-4bcb-afee-8473720639e4/mockup-of-a-crewneck-t-shirt-hanging-by-a-cement-wall-33686%20(3).png?v=1661394908318" alt="" className="firstImg" />
+          </div>
+          <div className="second grid">
+            <img src="https://cdn.glitch.global/6a19564e-7f95-4bcb-afee-8473720639e4/mockup-of-a-crewneck-t-shirt-hanging-by-a-cement-wall-33686%20(4).png?v=1661399589406" alt=""  className="secondImg"/>
+          </div>
+          <div className="third grid">
+          <img src="https://cdn.glitch.global/6a19564e-7f95-4bcb-afee-8473720639e4/mockup-of-a-crewneck-t-shirt-hanging-by-a-cement-wall-33686%20(2).png?v=1661394911996" alt="" className="thirdImg" />  
+          </div>
+          <div className="fourth grid">
+          <img src="https://cdn.glitch.global/6a19564e-7f95-4bcb-afee-8473720639e4/mockup-of-a-crewneck-t-shirt-hanging-by-a-cement-wall-33686.png?v=1661394926986" alt="" className="fourthImg" />
+          </div>
+        </div>
+      </div> */}
+
+      {/* <div className="design">
+        <span className="web">www.marvis.dev <br />www.fiverr.com/marvisdev</span>
+        <span className="td">T-Shirt <br /> Designs</span>
+        <div className="checker">
+          <div className="check">
+            <i class="fa-solid fa-check"></i>
+            </div>
+            <p className="text">Vector Graphic Design</p>
+        </div>
+
+        <div className="checker">
+          <div className="check">
+            <i class="fa-solid fa-check"></i>
+            </div>
+            <p className="text">3D Design</p>
+        </div>
+
+        <div className="checker">
+          <div className="check">
+            <i class="fa-solid fa-check"></i>
+            </div>
+            <p className="text">Calligraphy</p>
+        </div>
+
+        <div className="checker">
+          <div className="check">
+            <i class="fa-solid fa-check"></i>
+            </div>
+            <p className="text">Word Play</p>
+        </div>
+
+        <div className="checker">
+          <div className="check">
+            <i class="fa-solid fa-check"></i>
+            </div>
+            <p className="text">Realism</p>
+        </div>
+      </div> */}
+
+      {/* <div className="design">
+        <span className="text">design for your sweatshirts</span>
+        <div className="left"></div>
+        <div className="right"></div>
+      </div> */}
+
+      {/* <div className="design">
+        <div className="left-grid"></div>
+        <div className="right-grid">
+        <div className="top-grid"></div>
+        <div className="middle-grid"></div>
+        <div className="bottom-grid"></div>
+        </div>
+      </div> */}
     </div>
   );
 }
